cc71aed11f7282eb38a65d5a9e524ca609d9db30
alliefm <github@allie.fm>
2024-04-10 13:38:54 -0700

    Create Airports50.h

---
+++ Airports50.h
@@ -0,0 +1,56 @@
+#define NUM_AIRPORTS 50 // This is the number of entries in the airports.h file, including any NULL or KEY entries
+const char* ssid = "TX Beez";
+const char* password =  "221Crystal";
+
+std::vector<String> airports({
+"PHNL",  //01 Honolulu
+"NULL", //02  Not lit
+"NULL", //03 Not lit
+"KSNA",  //04 Anaheim/ Santa Ana
+"NULL", //05  Not lit
+"KSJC", //06 San Jose
+"NULL", //07
+"NULL", //08
+"KLAS", //09  Las Vegas
+"NULL", //10
+"KPHX", //11  Pheonix
+"NULL", //12
+"KABQ", //13  ALBUQUERQUE 
+"NULL", //14
+"KBIF", //15  Ft Bliss, El Paso
+"NULL", //16
+"NULL", //17
+"KSAT", //18  San Antonio
+"NULL", //19
+"KDFW", //20  Ft Worth
+"KOKC", //21 Okla City
+"KICT", //22 Whicita
+"KMKC", //23 Kansas City
+"NULL", //24 
+"KLIT", //25 Little Rock
+"KMEM", //26 Memphis
+"KJAN", //27 Jackson MI
+"KMSY", //28 New Orleans
+"KBFM", //29 Mobile AL
+"KMGM", //30 Montgomery AL
+"KTLH", //31 Talahassee FL
+"KJAX", //32 Jacksonville FL
+"KATL", //33 Atlanta GA
+"KCAE", //34 Columbia SC
+"KCLT", //35 Charlotte NC
+"KRDU", //36 Raleigh NC
+"KRIC", //37 Richmond VA
+"KDCA", //38 Washington DC
+"KPHL", //39 Philadelphia PA
+"KLGA", //40 NYC NY
+"KHFD", //41 Hardford 
+"KBOS", //42 Boston ME
+"KPWM", //43 Portland ME
+"KMPV", //44 Montpellier 
+"KALB", //45 Albany NY
+"KROC", //46 Rochester NY
+"KBUF", //47 Buffolo NY
+"NULL", //48 Pittsburgh PA
+"NULL", //49 Columbus OH
+"NULL"  //50
+});
