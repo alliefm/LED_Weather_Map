3a8e15b056257ba8a6d05076a6ead27bcd7cd71f
alliefm <github@allie.fm>
2024-08-03 14:40:15 -0700

    Create airports50NORCAL.h

---
+++ airports50NORCAL.h
@@ -0,0 +1,54 @@
+#define NUM_AIRPORTS 50 // This is the number of entries in the airports.h file, including any NULL or KEY entries
+
+std::vector<String> airports({
+  "LIFR", // 1 order of LEDs, starting with 1 should be KKIC; use VFR, WVFR, MVFR, IFR, LIFR for key; NULL for no airport
+  "IFR", // 2
+  "MVFR", // 3
+  "WVFR", // 4
+  "VFR", // 5
+  "KHAF", // 6
+  "KSFO", // 7
+  "KPAO", // 8 
+  "KSJC", // 9
+  "KWVI", // 10
+  "KSNS", // 11 
+  "KCVH", // 12 
+  "NULL", // 13 NULL
+  "KMAE", // 14
+  "KMCE", // 15
+  "KMOD", // 16
+  "KO22", // 17
+  "KSCK", // 18
+  "KC83", // 19 byron
+  "KLVK", // 20
+  "KOAK", // 21
+  "KCCR", // 22
+  "KAPC", // 23
+  "KVCB", // 24
+  "KSAC", // 25
+  "KPVF", // 26
+  "KAUN", // 27
+  "KTRK", // 28
+  "KGOO", // 29 //////
+  "KBAB", // 30
+  "KOVE", // 31
+  "KCIC", // 32
+  "KO05", // 33 
+  "KRBL", // 34
+  "KRDD", // 35
+  "KO86", // 36
+  "KMHS", // 37
+  "NULL", // 38 NULL
+  "KAAT", // 39 Alturas  
+  "NULL", // 40 NULL
+  "KSIY", // 41
+  "NULL", // 42 NULL
+  "KCEC", // 43
+  "KACV", // 44
+  "KFOT", // 45
+  "NULL", // 46 NULL
+  "KUKI", // 47
+  "KSTS", // 48
+  "NULL", // 49
+  "NULL"  // 50
+});
